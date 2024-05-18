427f65d8bda932dc88517b056c1947815c5c0840
vvaasd <egregr381@gmail.com>
2024-05-18 17:34:51 +0500

    undoing accidental merge

--- src/components/Icon/Svg/CloseSvg.jsx
+++
@@ -1,3 +0,0 @@
-export function CloseSvg({ ...props }) {
-  return <svg></svg>;
-}
--- src/constants/SLIDER_TYPES.js
+++
@@ -1,6 +0,0 @@
-const SLIDER_TYPES = {
-  forOneDay: 'forOneDay',
-  forFiveDays: 'forFiveDays',
-};
-
-export default SLIDER_TYPES;
